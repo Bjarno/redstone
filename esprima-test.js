7125750b71455a982cb038b66c8e6a6763c657b8
Bjarno Oeyen <mail@bjarno.be>
2016-02-13 16:18:37 +0100

    Removes -test.js files.

--- esprima-test.js
+++
@@ -1,8 +0,0 @@
-var esprima = require("esprima");
-
-var util = require('util');
-var dump = function dump(obj) {
-	console.log(util.inspect(obj, {showHidden: false, depth: null}));
-}
-
-dump(esprima.parse("hello(\"world\")"));
