6af1ba8e74387f74e3f91bff4060704891baeb51
Bjarno Oeyen <mail@bjarno.be>
2016-02-22 23:00:03 +0100

    Adds debug information during parsing.

--- redstone.js
+++ redstone.js
@@ -4,6 +4,10 @@ var preparer = require("./redstone-preparer.js");
 
 var ConverterContext = require("./redstone-types.js").ConverterContext;
 
+var dump = require("./utils.js").dump;
+
+var DEBUG = true;
+
 /**
  * Fills in the default values for an options object. It will create (and 
  * return) an empty options object with all default options, if an invalid
@@ -23,15 +27,53 @@ var preprocess_options = function preprocess_options(options) {
 	return options;
 };
 
+// TODO: JSDoc
+var head = function head(title) {
+	if (!(DEBUG)) { return; }
+
+	var len = 64;
+	var line = "=".repeat(len);
+	console.log("");
+	console.log(line);
+	console.log(" ".repeat((len-title.length)/2) + title);
+	console.log(line);
+	console.log("");
+}
+
+// TODO: JSDoc
+var subhead = function subhead(title) {
+	if (!(DEBUG)) { return; }
+	
+	var len = 64;
+	var line = "-".repeat(len);
+	console.log("");
+	console.log(line);
+	console.log(" ".repeat((len-title.length)/2) + title);
+	console.log(line);
+	console.log("");
+}
+
+// TODO: JSDoc
 var generate = function generate(input, options) {
 	options = preprocess_options(options);
 	var context = new ConverterContext([], [], options);
 
 	var result_parse = parser.parse(input);
+	head("Parse result");
+	dump(result_parse);
+
 	preparer.prepare(result_parse, context);
-	
+	head("Pre-process result");
+	subhead("Trees");
+	dump(result_parse);
+	subhead("Context");
+	dump(context);
+
 	preparer.applyContext(result_parse, context);
 	var result_html = generator.generate(result_parse, context);
+
+	head("Resulting HTML");
+	dump(result_html);
 
 	return {html: result_html, "context": context};
 };
