57088398b41274c66acc9b1764e5c93cf1f64f3f
Bjarno Oeyen <mail@bjarno.be>
2016-02-25 00:07:46 +0100

    Removes unnecessary console.log in redstone-test.js

--- redstone-test.js
+++ redstone-test.js
@@ -2,4 +2,4 @@ var redstone = require("./redstone.js");
 var utils = require("./utils.js");
 
 var input = utils.readFile("input2");
-utils.dump(redstone.generate(input));
+redstone.generate(input);
