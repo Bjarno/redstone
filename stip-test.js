8c434dd794b74112d175ab143cc948bd5f25dc1a
Bjarno Oeyen <mail@bjarno.be>
2016-02-24 16:54:34 +0100

    Makes Stip.js working

--- stip-test.js
+++ stip-test.js
@@ -1,13 +1,17 @@
+var escodegen = require("escodegen");
 var tiersplit = require("./../jspdg/stip/tiersplit.js").tiersplit;
+var head = require("./utils.js").head;
+var subhead = require("./utils.js").subhead;
+
 var result = tiersplit("/* @server */ {function foo() {/*@reply */ bar(3)}} /* @client */ {function bar(y) {return 42+y;} foo();}");
 
-console.log("\n\n\n\n");
-console.log("==============");
-console.log("\n\n\n\n");
+var clientJS = escodegen.generate(result[0].program);
+var serverJS = escodegen.generate(result[1].program);
 
-console.log(result);
+head("Result");
 
-/*
-var dump = require("./utils.js").dump;
-dump(result);
-*/
+subhead("Client");
+console.log(clientJS);
+
+subhead("Server");
+console.log(serverJS);
