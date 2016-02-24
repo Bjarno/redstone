ad6c00c88491865a4f4362ad29395c479f5a9ef9
Bjarno Oeyen <mail@bjarno.be>
2016-02-24 17:07:41 +0100

    Now links to forked copy of jspdg

--- redstone-test.js
+++ redstone-test.js
@@ -1,5 +1,5 @@
 var redstone = require("./redstone.js");
 var utils = require("./utils.js");
 
-var input = utils.readFile("input2.redstone");
+var input = utils.readFile("input.redstone");
 utils.dump(redstone.generate(input));
