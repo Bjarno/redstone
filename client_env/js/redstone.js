af2a0c004f9f9bb19afb02accdc2d18e6473ebc1
Bjarno Oeyen <mail@bjarno.be>
2016-04-08 13:31:16 +0200

    Preperation step of handling different type of expressions to update in GUI

--- client_env/js/redstone.js
+++ client_env/js/redstone.js
@@ -1,4 +1,6 @@
 function RUpdateGUI(idname, newvalue) {
+	// TODO: Look up type and update accordingly
+	
 	var cleanupOldValue = function (oldvalue) {
 		if (typeof oldvalue == 'object') {
 			OBJSPY.untrack(oldvalue, idname);
