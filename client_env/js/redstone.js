a31f9641f0ed88a522acc5784c9e56a056b09b3a
Bjarno Oeyen <mail@bjarno.be>
2016-04-08 03:43:41 +0200

    Untested implementation of dynamic watching for internal object changes, needs test cases

--- client_env/js/redstone.js
+++ client_env/js/redstone.js
@@ -1,4 +1,25 @@
 function RUpdateGUI(idname, newvalue) {
-	// TODO: Use Objwatch if object changes state
-	ractive.set(idname, newvalue);
+
+	var cleanupOldValue = function (oldvalue) {
+		if (typeof oldvalue == 'object') {
+			OBJSPY.untrack(oldvalue, idname);
+		}
+	};
+
+	var update = function (newvalue) {
+		cleanupOldValue(ractive.get(idname));
+		ractive.set(idname, newvalue);
+	};
+
+	update(newvalue);
+
+	if (typeof newvalue == 'object') {
+		OBJSPY.track(
+				newvalue,
+				function (prop, action, difference, oldvalue, fullnewvalue) {
+					update(fullnewvalue);
+				},
+				idname
+		);
+	}
 }
