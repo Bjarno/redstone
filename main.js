42bb3e661f529ed11d1fc0bff4bc2b9f17806623
Bjarno Oeyen <mail@bjarno.be>
2016-04-09 19:08:45 +0200

    Adds comments in main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+/************************************/
+/* Command line version of Redstone */
+/************************************/
+
 var redstone = require("./redstone.js");
 var utils    = require("./utils.js");
 var fs       = require("fs");
 var path     = require("path");
 
+// Load given file as argument from CLI
 var inputFile = process.argv[2];
 
 if (inputFile === undefined) {
@@ -10,6 +15,7 @@ if (inputFile === undefined) {
 	process.exit(1);
 }
 
+// Read the file from disk
 try {
 	var stats = fs.statSync(inputFile);
 } catch (e) {
@@ -17,12 +23,15 @@ try {
 	process.exit(1);
 }
 
+// Run Redstone tool
 var input = utils.readFile(inputFile);
 var result = redstone.generate(input);
 var client = result.client;
 var server = result.server;
 
+// Output the result in files
 utils.writeFile("server_env/server.js", server);
 utils.writeFile("client_env/index.html", client);
 
+// Exit
 process.exit(0);
