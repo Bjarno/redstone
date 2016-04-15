59e1a022408ad8d09715645d813cafd11c9f5afc
Bjarno Oeyen <mail@bjarno.be>
2016-04-15 14:12:25 +0200

    Bug fix in own client javascript libraries

--- client_env/js/objspy.js
+++ client_env/js/objspy.js
@@ -2,116 +2,120 @@
 // Will keep track of a list of watchers (listeners/observers), and cleans up watching
 // when there are no observers left.
 
-var debug = false;
+(function() {
 
-var OBJSPY = {};
+	var debug = false;
 
-var createWatcher = function(obj) {
-	var fullValue = obj;
+	OBJSPY = {};
 
-	var watcher = function (prop, action, difference, oldvalue) {
-		// Call listeners
-		if (debug) {
-			console.log("Detected update!");
-			console.log("Prop: ");
-			console.log(prop);
-			console.log("Action: ");
-			console.log(action);
-			console.log("Difference: ");
-			console.log(difference);
-			console.log("Oldvalue: ");
-			console.log(oldvalue);
-			console.log("Fullnewvalue: ");
-			console.log(this);
-		}
+	var createWatcher = function (obj) {
+		var fullValue = obj;
 
-		fullValue.__objspy__.listeners.forEach(function (keyandlistener) {
-			var listener = keyandlistener.listener;
-			listener(prop, action, difference, oldvalue, fullValue);
-		});
+		var watcher = function (prop, action, difference, oldvalue) {
+			// Call listeners
+			if (debug) {
+				console.log("Detected update!");
+				console.log("Prop: ");
+				console.log(prop);
+				console.log("Action: ");
+				console.log(action);
+				console.log("Difference: ");
+				console.log(difference);
+				console.log("Oldvalue: ");
+				console.log(oldvalue);
+				console.log("Fullnewvalue: ");
+				console.log(this);
+			}
 
-		if (debug) {
-			console.log("-----------------------------------------------------------");
-		}
+			fullValue.__objspy__.listeners.forEach(function (keyandlistener) {
+				var listener = keyandlistener.listener;
+				listener(prop, action, difference, oldvalue, fullValue);
+			});
+
+			if (debug) {
+				console.log("-----------------------------------------------------------");
+			}
+		};
+
+		return watcher;
 	};
 
-	return watcher;
-};
+	OBJSPY.track = function (obj, func, givenKey) {
+		var startWatch = false;
+		var key, idx;
 
-OBJSPY.track = function(obj, func, givenKey) {
-	var startWatch = false;
-	var key, idx;
+		var desc = Object.getOwnPropertyDescriptor(obj, '__objspy__');
+		if (desc === undefined) {
+			key = (givenKey === undefined) ? makeId() : givenKey;
+			Object.defineProperty(obj, '__objspy__', {
+				enumerable: false,
+				configurable: true,
+				writable: false,
+				value: {
+					listeners: [{
+						key: key,
+						listener: func
+					}]
+				}
+			});
+			startWatch = true;
+		} else {
+			if (givenKey === undefined) {
+				do {
+					key = makeId();
+					idx = -1;
+					obj.__objspy__.listeners.forEach(function (keyandlistener, i) {
+						if (keyandlistener.key === key) {
+							idx = i;
+						}
+					});
+				} while (idx !== -1);
+			} else {
+				// Generate really unique key
+				key = givenKey;
+			}
 
-	var desc = Object.getOwnPropertyDescriptor(obj, '__objspy__');
-	if (desc === undefined) {
-		key = (givenKey === undefined) ? makeId() : givenKey;
-		Object.defineProperty(obj, '__objspy__', {
-			enumerable: false,
-			configurable: true,
-			writable: false,
-			value: {
-				listeners: [{
-					key: key,
-					listener: func
-				}]
+			obj.__objspy__.listeners.push({
+				key: key,
+				listener: func
+			});
+		}
+
+		var watcher = createWatcher(obj);
+
+		if (startWatch) {
+			watch(obj, watcher, Infinity, true);
+		}
+
+		return key;
+	};
+
+	OBJSPY.untrack = function (obj, key) {
+		var idx = -1;
+		obj.__objspy__.listeners.forEach(function (keyandlistener, i) {
+			if (keyandlistener.key === key) {
+				idx = i;
 			}
 		});
-		startWatch = true;
-	} else {
-		if (givenKey === undefined) {
-			do {
-				key = makeId();
-				idx = -1;
-				obj.__objspy__.listeners.forEach(function (keyandlistener, i) {
-					if (keyandlistener.key === key) {
-						idx = i;
-					}
-				});
-			} while (idx !== -1);
-		} else {
-			// Generate really unique key
-			key = givenKey;
+
+		if (idx === -1) {
+			return false;
 		}
 
-		obj.__objspy__.listeners.push({
-			key: key,
-			listener: func
-		});
-	}
+		// Remove element
+		obj.__objspy__.listeners.splice(idx, 1);
 
-	var watcher = createWatcher(obj);
-
-	if (startWatch) {
-		watch(obj, watcher, Infinity, true);
-	}
-
-	return key;
-};
-
-OBJSPY.untrack = function(obj, key) {
-	var idx = -1;
-	obj.__objspy__.listeners.forEach(function (keyandlistener, i) {
-		if (keyandlistener.key === key) {
-			idx = i;
+		// "Half" Cleanup
+		if (obj.__objspy__.listeners.length === 0) {
+			if (debug) {
+				console.log("Stopped tracking an object.");
+				console.log(obj);
+			}
+			unwatch(obj);
 		}
-	});
+		// We could remove __objspy__ but it can safely remain there with an empty array, not that huge waste of space.
 
-	if (idx === -1) {
-		return false;
-	}
+		return true;
+	};
 
-	// Remove element
-	obj.__objspy__.listeners.splice(idx, 1);
-
-	// "Half" Cleanup
-	if (obj.__objspy__.listeners.length === 0) {
-		if (debug) {
-			console.log("Stopped tracking an object.");
-			console.log(obj);
-		}
-		unwatch(obj);
-	}
-	// We could remove __objspy__ but it can safely remain there with an empty array, not that huge waste of space.
-
-	return true;
-}
+})();
--- client_env/js/redstone.js
+++ client_env/js/redstone.js
@@ -1,257 +1,281 @@
-var updateCrumb = function updateCrumb(crumb, newValue) {
-	ractive.set(crumb.idName, newValue);
-};
+(function() {
+	var updateCrumb = function updateCrumb(crumb, newValue) {
+		ractive.set(crumb.idName, newValue);
+	};
 
-var variableInfo = {};
-var loaded = false;
-var waitingUpdates = [];
+	var variableInfo = {};
+	var loaded = false;
+	var waitingUpdates = [];
 
-var evalProgram = function evalProgram(program) {
-	var bodyLength = program.body.length;
-	if (bodyLength != 1) {
-		console.log("!!! Program.body.length should be equal to 1");
-		console.log("!!! Got " + bodyLength);
-		return false;
-	}
-	return eval(program.body[0]);
-};
-
-var evalExpressionStatement = function evalExpressionStatement(expressionStatement) {
-	return eval(expressionStatement.expression);
-};
-
-var evalLiteral = function evalLiteral(literal) {
-	return literal.value;
-};
-
-var evalIdentifier = function evalIdentifier(identifier) {
-	if (identifier.hasOwnProperty("isInCrumb")) {
-		return variableInfo[identifier.name].value;
-	} else {
-		console.log("!!! I don't know what to do with identifier that is not in crumb");
-		return false;
-	}
-};
-
-var evalMemberExpression = function evalMemberExpression(memberExpression) {
-	var object = eval(memberExpression.object);
-
-	if (memberExpression.computed) {
-		return object[eval(memberExpression.property)];
-	} else {
-		// If not computed, property has type identifier according to specification
-		return object[memberExpression.property.name];
-	}
-};
-
-var evalBinaryExpression = function evalBinaryExpression(binaryExpression) {
-	var left = eval(binaryExpression.left);
-	var right = eval(binaryExpression.right);
-	var operator = binaryExpression.operator;
-
-	switch (operator) {
-		case "==":          return left == right;
-		case "===":         return left === right;
-		case "!=":          return left != right;
-		case "!==":         return left !== right;
-		case "<":           return left < right;
-		case "<=":          return left <= right;
-		case ">":           return left > right;
-		case ">=":	        return left >= right;
-		case "<<":          return left << right;
-		case ">>":	        return left >> right;
-		case ">>>":         return left >>> right;
-		case "+":           return left + right;
-		case "-":           return left - right;
-		case "*":           return left * right;
-		case "/":           return left / right;
-		case "%":           return left % right;
-		case "|":           return left | right;
-		case "^":           return left ^ right;
-		case "&":           return left & right;
-		case "in":          return left in right;
-		case "instanceof":  return left instanceof right;
-
-		default:
-			console.log("!!! Unknown type of BinaryOperator: " + operator);
+	var evalProgram = function evalProgram(program) {
+		var bodyLength = program.body.length;
+		if (bodyLength != 1) {
+			console.log("!!! Program.body.length should be equal to 1");
+			console.log("!!! Got " + bodyLength);
 			return false;
-	}
-};
-
-var evalCallExpression = function evalCallExpression(callExpression) {
-	var callee = callExpression.callee;
-	var methodObj;
-	var thisObj = null;
-
-	switch (callee.type) {
-		case esprima.Syntax.Identifier:
-			methodObj = METHODS[callee.name];
-
-			if (!methodObj) {
-				console.log("!!! method object undefined for " + callee.name);
-				return false;
-			}
-			break;
-		
-		case esprima.Syntax.MemberExpression:
-			if (callee.computed) {
-				console.log("!!! computed NYI");
-				return false;
-			}
-
-			thisObj = eval(callee.object);
-			methodObj = thisObj[callee.property.name];
-			break;
-		
-		default:
-			console.log("!!! Unknown type of callExpression callee: " + callee.type);
-			return false;
-	}
-
-	var argumentExpressions = callExpression.arguments;
-	var arguments = argumentExpressions.map(eval);
-
-	return methodObj.apply(thisObj, arguments);
-};
-
-var eval = function eval(ast) {
-	var type = ast.type;
-
-	switch (type) {
-		case esprima.Syntax.Program:
-			return evalProgram(ast);
-
-		case esprima.Syntax.ExpressionStatement:
-			return evalExpressionStatement(ast);
-
-		case esprima.Syntax.Literal:
-			return evalLiteral(ast);
-
-		case esprima.Syntax.Identifier:
-			return evalIdentifier(ast);
-
-		case esprima.Syntax.MemberExpression:
-			return evalMemberExpression(ast);
-
-		case esprima.Syntax.BinaryExpression:
-			return evalBinaryExpression(ast);
-
-		case esprima.Syntax.CallExpression:
-			return evalCallExpression(ast);
-
-		default:
-			console.log("!!! unknown type of expression: " + type);
-			return false;
-	}
-};
-
-var updateVariable = function updateVariable(variableName, value) {
-	// When not yet loaded, wait until loaded
-	if (!loaded) {
-		waitingUpdates.push({
-			variableName: variableName,
-			value: value
-		});
-		return;
-	}
-
-	// Create info object if not yet created
-	if (!variableInfo.hasOwnProperty(variableName)) {
-		variableInfo[variableName] = {
-			value: undefined,
-			blocked: false,
-			finalValue : value
 		}
-	}
+		return eval(program.body[0]);
+	};
 
-	// Don't do anything if blocked
-	if (variableInfo[variableName].blocked) {
-		console.log("!!! Variable " + variableName + " is blocked, not allowing nested updating GUI on same variable!");
+	var evalExpressionStatement = function evalExpressionStatement(expressionStatement) {
+		return eval(expressionStatement.expression);
+	};
 
-		// The new value is stored in finalValue
+	var evalLiteral = function evalLiteral(literal) {
+		return literal.value;
+	};
+
+	var evalIdentifier = function evalIdentifier(identifier) {
+		if (identifier.hasOwnProperty("isInCrumb")) {
+			return variableInfo[identifier.name].value;
+		} else {
+			console.log("!!! I don't know what to do with identifier that is not in crumb");
+			return false;
+		}
+	};
+
+	var evalMemberExpression = function evalMemberExpression(memberExpression) {
+		var object = eval(memberExpression.object);
+
+		if (memberExpression.computed) {
+			return object[eval(memberExpression.property)];
+		} else {
+			// If not computed, property has type identifier according to specification
+			return object[memberExpression.property.name];
+		}
+	};
+
+	var evalBinaryExpression = function evalBinaryExpression(binaryExpression) {
+		var left = eval(binaryExpression.left);
+		var right = eval(binaryExpression.right);
+		var operator = binaryExpression.operator;
+
+		switch (operator) {
+			case "==":
+				return left == right;
+			case "===":
+				return left === right;
+			case "!=":
+				return left != right;
+			case "!==":
+				return left !== right;
+			case "<":
+				return left < right;
+			case "<=":
+				return left <= right;
+			case ">":
+				return left > right;
+			case ">=":
+				return left >= right;
+			case "<<":
+				return left << right;
+			case ">>":
+				return left >> right;
+			case ">>>":
+				return left >>> right;
+			case "+":
+				return left + right;
+			case "-":
+				return left - right;
+			case "*":
+				return left * right;
+			case "/":
+				return left / right;
+			case "%":
+				return left % right;
+			case "|":
+				return left | right;
+			case "^":
+				return left ^ right;
+			case "&":
+				return left & right;
+			case "in":
+				return left in right;
+			case "instanceof":
+				return left instanceof right;
+
+			default:
+				console.log("!!! Unknown type of BinaryOperator: " + operator);
+				return false;
+		}
+	};
+
+	var evalCallExpression = function evalCallExpression(callExpression) {
+		var callee = callExpression.callee;
+		var methodObj;
+		var thisObj = null;
+
+		switch (callee.type) {
+			case esprima.Syntax.Identifier:
+				methodObj = METHODS[callee.name];
+
+				if (!methodObj) {
+					console.log("!!! method object undefined for " + callee.name);
+					return false;
+				}
+				break;
+
+			case esprima.Syntax.MemberExpression:
+				if (callee.computed) {
+					console.log("!!! computed NYI");
+					return false;
+				}
+
+				thisObj = eval(callee.object);
+				methodObj = thisObj[callee.property.name];
+				break;
+
+			default:
+				console.log("!!! Unknown type of callExpression callee: " + callee.type);
+				return false;
+		}
+
+		var argumentExpressions = callExpression.arguments;
+		var arguments = argumentExpressions.map(eval);
+
+		return methodObj.apply(thisObj, arguments);
+	};
+
+	var eval = function eval(ast) {
+		var type = ast.type;
+
+		switch (type) {
+			case esprima.Syntax.Program:
+				return evalProgram(ast);
+
+			case esprima.Syntax.ExpressionStatement:
+				return evalExpressionStatement(ast);
+
+			case esprima.Syntax.Literal:
+				return evalLiteral(ast);
+
+			case esprima.Syntax.Identifier:
+				return evalIdentifier(ast);
+
+			case esprima.Syntax.MemberExpression:
+				return evalMemberExpression(ast);
+
+			case esprima.Syntax.BinaryExpression:
+				return evalBinaryExpression(ast);
+
+			case esprima.Syntax.CallExpression:
+				return evalCallExpression(ast);
+
+			default:
+				console.log("!!! unknown type of expression: " + type);
+				return false;
+		}
+	};
+
+	var updateVariable = function updateVariable(variableName, value) {
+		// When not yet loaded, wait until loaded
+		if (!loaded) {
+			waitingUpdates.push({
+				variableName: variableName,
+				value: value
+			});
+			return;
+		}
+
+		// Create info object if not yet created
+		if (!variableInfo.hasOwnProperty(variableName)) {
+			variableInfo[variableName] = {
+				value: undefined,
+				blocked: false,
+				finalValue: value
+			}
+		}
+
+		// Don't do anything if blocked
+		if (variableInfo[variableName].blocked) {
+			console.log("!!! Variable " + variableName + " is blocked, not allowing nested updating GUI on same variable!");
+
+			// The new value is stored in finalValue: when variable is unblocked, the final value is used for storing, so
+			// it reflects the final value as stored in client layer
+			variableInfo[variableName].finalValue = value;
+			return false;
+		}
+
+		// Clean up old value
+		var oldValue = variableInfo[variableName].value;
+		if (typeof oldvalue == 'object') {
+			OBJSPY.untrack(oldvalue, variableName);
+		}
+
+		// Block and set new value
+		variableInfo[variableName].blocked = true;
 		variableInfo[variableName].finalValue = value;
-		return false;
-	}
+		variableInfo[variableName].value = value;
 
-	// Clean up old value
-	var oldValue = variableInfo[variableName].value;
-	if (typeof oldvalue == 'object') {
-		OBJSPY.untrack(oldvalue, variableName);
-	}
+		var crumbIds = VARTOCRUMBID[variableName];
 
-	// Block and set new value
-	variableInfo[variableName].blocked = true;
-	variableInfo[variableName].finalValue = value;
-	variableInfo[variableName].value = value;
+		var onInternalUpdate = function onInternalUpdate() {
+			crumbIds.map(function (crumbId) {
+				return CRUMBS[crumbId];
+			}).forEach(function (crumb) {
+				var value = eval(crumb.parsedExpression);
+				updateCrumb(crumb, value);
+			});
+		};
 
-	var crumbIds = VARTOCRUMBID[variableName];
+		// Do initial update
+		onInternalUpdate();
 
-	var onInternalUpdate = function onInternalUpdate() {
+		// Unblock
+		variableInfo[variableName].blocked = false;
+
+		// Set the final value
+		value = variableInfo[variableName].finalValue;
+		variableInfo[variableName].value = value;
+
+		// Track value
+		if (typeof value == 'object') {
+			OBJSPY.track(
+				value,
+				function (prop, action, difference, oldvalue, fullnewvalue) {
+					onInternalUpdate();
+				},
+				variableName
+			);
+		}
+
+		return true;
+	};
+
+	var initGUI = function initGUI() {
+		loaded = true;
+
+		// Evaluate crumbs without any varnames
+		var crumbIds = Object.keys(CRUMBS);
 		crumbIds.map(function (crumbId) {
 			return CRUMBS[crumbId];
 		}).forEach(function (crumb) {
-			var value = eval(crumb.parsedExpression);
-			updateCrumb(crumb, value);
+			var variableNames = crumb.variableNames;
+
+			// Immediate evaluate after loading
+			if (variableNames.length === 0) {
+				var value = eval(crumb.parsedExpression);
+				updateCrumb(crumb, value);
+			}
 		});
+
+		// Evaluate those that were waiting until loaded
+		for (var i = 0; i < waitingUpdates.length; i++) {
+			var upd = waitingUpdates[i];
+			updateVariable(upd.variableName, upd.value);
+		}
+		waitingUpdates = [];
 	};
 
-	// Do initial update
-	onInternalUpdate();
+	var init = function init() {
+		initGUI();
+	};
 
-	// Unblock
-	variableInfo[variableName].blocked = false;
+	REDSTONE = {};
 
-	// Set the final value
-	value = variableInfo[variableName].finalValue;
-	variableInfo[variableName].value = value;
-
-	// Track value
-	if (typeof value == 'object') {
-		OBJSPY.track(
-			value,
-			function (prop, action, difference, oldvalue, fullnewvalue) {
-				onInternalUpdate();
-			},
-			variableName
-		);
-	}
-
-	return true;
-};
-
-var initGUI = function initGUI() {
-	loaded = true;
-
-	// Evaluate crumbs without any varnames
-	var crumbIds = Object.keys(CRUMBS);
-	crumbIds.map(function(crumbId) {
-		return CRUMBS[crumbId];
-	}).forEach(function (crumb) {
-		var variableNames = crumb.variableNames;
-
-		// Immediate evaluate after loading
-		if (variableNames.length === 0) {
-			var value = eval(crumb.parsedExpression);
-			updateCrumb(crumb, value);
-		}
-	});
-
-	// Evaluate those that were waiting until loaded
-	for (var i = 0; i < waitingUpdates.length; i++) {
-		var upd = waitingUpdates[i];
-		updateVariable(upd.variableName, upd.value);
-	}
-	waitingUpdates = [];
-};
-
-var init = function init() {
-	initGUI();
-};
-
-REDSTONE = {};
-
-REDSTONE.init = init;
-REDSTONE.updateVariable = updateVariable;
-REDSTONE.getVarInfo = function(varname) {
-	return variableInfo[varname];
-};
+	REDSTONE.init = init;
+	REDSTONE.updateVariable = updateVariable;
+	REDSTONE.getVarInfo = function (varname) {
+		return variableInfo[varname];
+	};
+})();
