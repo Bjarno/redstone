Bjarno/redstone

commit e406e71cb7f5a88b4b71912479a1470a5bf8388d

Allows computed property names for MemberExpressions

input.redstone
@@ -60,7 +60,7 @@
 		return bar * 2;
 	}
 
-	// TODO: Just use serverCount (?)
+	// TODO: Use a shared variable (stores)
 	var count = 0;
     function clickIncrease() { sendIncrease(); }
     function handshake() { sendCounter(); }
@@ -94,6 +94,8 @@ body
 		p I don't have smiley
 	p.
 		Last message: {{last.message}} by {{last.author + "a"}}.
+	p.
+		Last author only: {{last["author"]}}
 	p.
 		1 + 2 = {{1+2}}
 	p.

redstone-preparer.js
@@ -57,21 +57,31 @@ var is_in_each = function is_in_each() {
  * @param {MemberExpression} expression The expression to find the combination of the variable name (most-left) and the property hierarchy that is
  * requested from the MemberExpression.
  * @private
- * @returns {String} containing the variable name (key: varname) and the ordered hierarchy of accessed properties (key: properties)
+ * @returns {Array} Array containing variable top-level variables name of things inside a MemberExpression
  */
 var parse_memberexpression_varname = function parse_memberexpression_varname(expression) {
+    var result = [];
+
     switch (expression.type) {
         case esprima.Syntax.Identifier:
             expression.isInCrumb = true; // While evaluating, make sure we know this identifier name should be looked up locally
-            return expression.name;
+            result.push(expression.name);
+            break;
 
         case esprima.Syntax.MemberExpression:
             // Get the next level
-            return parse_memberexpression_varname(expression.object);
+            if (expression.computed) {
+                result = result.concat(find_varnames_expression(expression.property));
+            }
+            
+            result = result.concat(parse_memberexpression_varname(expression.object));
+            break;
 
         default:
             throw "Only supports identifiers (or nested MemberExpressions) for MemberExpression's object.";
     }
+
+    return result;
 };
 
 /**

client_env/js/redstone.js
@@ -37,8 +37,7 @@ var evalMemberExpression = function evalMemberExpression(memberExpression) {
 	var object = eval(memberExpression.object);
 
 	if (memberExpression.computed) {
-		console.log("!!! computed NYI");
-		return false;
+		return object[eval(memberExpression.property)];
 	} else {
 		return object[memberExpression.property.name];
 	}

commit a9cbaa8018a58bfe757d688c145c64567d77e8d5

Fixes bug introduced in e406e71cb7f5a88b4b71912479a1470a5bf8388d

input.redstone
@@ -25,7 +25,7 @@
 	    messages  = [],
 	    testObj   = {};
 
-	// var hassmiley = false;
+	var hassmiley = false;
 
 	testObj.a = 0;
 	testObj.display = function() {
@@ -51,7 +51,7 @@
 		m.message = message;
 		messages.push(m);
 
-		// smiley = false;
+		smiley = false;
 	}
 
 	var bar = 123;

redstone-preparer.js
@@ -73,7 +73,7 @@ var parse_memberexpression_varname = function parse_memberexpression_varname(exp
             if (expression.computed) {
                 result = result.concat(find_varnames_expression(expression.property));
             }
-            
+
             result = result.concat(parse_memberexpression_varname(expression.object));
             break;
 
@@ -103,7 +103,7 @@ var find_varnames_expression = function find_varnames_expression(expression) {
             break;
 
         case esprima.Syntax.MemberExpression:
-            result.push(parse_memberexpression_varname(expression));
+            result = result.concat(parse_memberexpression_varname(expression));
             break;
 
         case esprima.Syntax.BinaryExpression:
@@ -121,7 +121,7 @@ var find_varnames_expression = function find_varnames_expression(expression) {
                     break;
 
                 case esprima.Syntax.MemberExpression:
-                    result.push(parse_memberexpression_varname(calleeExpression));
+                    result = result.concat(parse_memberexpression_varname(calleeExpression));
                     break;
             }
 